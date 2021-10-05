fbfb0dca5b699c8c388943b29700743f07c999f6
Drew Bollinger <drew@developmentseed.org>
2021-10-05 10:52:14 -0400

    move delayed methods to functions

--- label_maker_dask/main.py
+++ label_maker_dask/main.py
@@ -22,6 +22,70 @@ from label_maker_dask.utils import (
 )
 
 
+# delayed functions
+# not methods to avoid serialization and large object issues
+@dask.delayed
+def tile_to_label(tile: Tile, ml_type: str, classes: Dict, label_source: str):
+    """
+    Parameters
+    ------------
+    tile: mercantile.Tile
+        tile index
+    ml_type: str
+    classes: Dict
+    label_source: str
+    Returns
+    ---------
+    label: tuple
+        The first element is a mercantile tile. The second element is a numpy array
+        representing the label of the tile
+    """
+
+    url = label_source.format(x=tile.x, y=tile.y, z=tile.z)
+    r = requests.get(url)
+    r.raise_for_status()
+
+    tile_data = mapbox_vector_tile.decode(r.content)
+    features = [
+        project_feat(feat, tile.x, tile.y, tile.z, EXTENT)
+        for feat in tile_data["osm"]["features"]
+        if "Multi" not in feat["geometry"]["type"]
+    ]
+
+    clip_mask = Polygon(((0, 0), (0, 255), (255, 255), (255, 0), (0, 0)))
+    geos = []
+    for feat in features:
+        for i, cl in enumerate(classes):
+            ff = create_filter(cl.get("filter"))
+            if ff(feat):
+                geo = shape(feat["geometry"])
+                try:
+                    geo = geo.intersection(clip_mask)
+                except TopologicalError as e:
+                    print(e, "skipping")
+                    break
+                if cl.get("buffer"):
+                    geo = geo.buffer(cl.get("buffer"), 4)
+                if not geo.is_empty:
+                    geos.append((mapping(geo), i + 1))
+
+    w, n = ul(tile.x, tile.y, tile.z)
+    resolution = degrees_per_pixel(tile.z, 0)
+    transform = Affine(resolution, 0, w, 0, -resolution, n)
+
+    label = rasterize(geos, out_shape=(256, 256), transform=transform)
+
+    return (tile, label)
+
+
+@dask.delayed
+def get_image(tup, imagery):
+    """fetch images"""
+    tile, label = tup
+    image_function = get_image_function(imagery)
+    return Result(tile, label, image_function(tile, imagery))
+
+
 class Result:
     """TODO: params, return; class for label results"""
 
@@ -54,7 +118,6 @@ class LabelMakerJob:
         imagery: str = None,
         label_source: str = None,
         ml_type: str = None,
-        cluster=None,
     ):
         """initialize new label maker job for dask"""
         self.zoom = zoom
@@ -63,14 +126,16 @@ class LabelMakerJob:
         self.imagery = imagery
         self.label_source = label_source
         self.ml_type = ml_type
-        self.cluster = cluster
         self.results = None
 
     def build_job(self):
         """create task list for labels and images"""
         self.tile_list = list(tiles(*self.bounds, [self.zoom]))
-        label_tups = [self.tile_to_label(tile) for tile in self.tile_list]
-        self.tasks = [self.get_image(tup) for tup in label_tups]
+        label_tups = [
+            tile_to_label(tile, self.ml_type, self.classes, self.label_source)
+            for tile in self.tile_list
+        ]
+        self.tasks = [get_image(tup, self.imagery) for tup in label_tups]
         print("Sample graph")
         return dask.visualize(self.tasks[:3])
 
@@ -85,64 +150,3 @@ class LabelMakerJob:
     def execute_job(self):
         """compute the labels and images"""
         self.results = dask.compute(*self.tasks)
-
-    @dask.delayed
-    def tile_to_label(self, tile):
-        """
-        Parameters
-        ------------
-        tile: mercantile.Tile
-            tile index
-        label_job: dict
-            Job definition
-        Returns
-        ---------
-        label: tuple
-            The first element is a mercantile tile. The second element is a numpy array
-            representing the label of the tile
-        """
-        ml_type = self.ml_type  # noqa: F841
-        classes = self.classes
-
-        url = self.label_source.format(x=tile.x, y=tile.y, z=tile.z)
-        r = requests.get(url)
-        r.raise_for_status()
-
-        tile_data = mapbox_vector_tile.decode(r.content)
-        features = [
-            project_feat(feat, tile.x, tile.y, tile.z, EXTENT)
-            for feat in tile_data["osm"]["features"]
-            if "Multi" not in feat["geometry"]["type"]
-        ]
-
-        clip_mask = Polygon(((0, 0), (0, 255), (255, 255), (255, 0), (0, 0)))
-        geos = []
-        for feat in features:
-            for i, cl in enumerate(classes):
-                ff = create_filter(cl.get("filter"))
-                if ff(feat):
-                    geo = shape(feat["geometry"])
-                    try:
-                        geo = geo.intersection(clip_mask)
-                    except TopologicalError as e:
-                        print(e, "skipping")
-                        break
-                    if cl.get("buffer"):
-                        geo = geo.buffer(cl.get("buffer"), 4)
-                    if not geo.is_empty:
-                        geos.append((mapping(geo), i + 1))
-
-        w, n = ul(tile.x, tile.y, tile.z)
-        resolution = degrees_per_pixel(tile.z, 0)
-        transform = Affine(resolution, 0, w, 0, -resolution, n)
-
-        label = rasterize(geos, out_shape=(256, 256), transform=transform)
-
-        return (tile, label)
-
-    @dask.delayed
-    def get_image(self, tup):
-        """fetch images"""
-        tile, label = tup
-        image_function = get_image_function(self.imagery)
-        return Result(tile, label, image_function(tile, self.imagery))
